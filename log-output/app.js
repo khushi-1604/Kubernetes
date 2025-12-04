87cc9ce4fcaa7ad4af8e7f4d247efa575179fc2c
Khushi Shukla <khushiyshukla@gmail.com>
2025-12-04 21:27:24 +0530

    coompleted task 1.7 - Log Output Status + Ingress

--- log-output/app.js
+++ log-output/app.js
@@ -1,10 +1,22 @@
-const crypto = require("crypto");
+const express = require("express");
+const app = express();
+const { v4: uuidv4 } = require("uuid");
 
-const randomId = crypto.randomUUID();
+const randomString = uuidv4();
 
-console.log("App started. Random ID:", randomId);
+app.get("/status", (req, res) => {
+  res.json({
+    timestamp: new Date().toISOString(),
+    randomString: randomString
+  });
+});
 
+// Keep previous logging every 5 seconds (if still needed)
 setInterval(() => {
-    const timestamp = new Date().toISOString();
-    console.log(`${timestamp}: ${randomId}`);
+  console.log(`${new Date().toISOString()}: ${randomString}`);
 }, 5000);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`);
+});
